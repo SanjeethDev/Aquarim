10cdecb80b1bcd215dd827a9f76f35eaed89868f
SanjeethDev <91370632+SanjeethDev@users.noreply.github.com>
2024-08-22 13:47:32 +0530

    feat!:  - add ability to delete in item popup

--- app/src/main/java/com/sanjeethdev/aquarim/RecordItemPopUp.java
+++ app/src/main/java/com/sanjeethdev/aquarim/RecordItemPopUp.java
@@ -1,23 +1,11 @@
 package com.sanjeethdev.aquarim;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.LinearLayout;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.sanjeethdev.aquarim.databinding.ActivityMainBinding;
 import com.sanjeethdev.aquarim.databinding.ActivityRecordItemPopUpBinding;
-
 import java.util.Objects;
 
 public class RecordItemPopUp extends AppCompatActivity
@@ -40,16 +28,28 @@ public class RecordItemPopUp extends AppCompatActivity
 
         binding.recordPopupDelete.setOnClickListener(view ->
         {
-            LiquidRecordDbHelper recordDbHelper = new LiquidRecordDbHelper(this);
-            if (recordDbHelper.deleteRecord(bundle.getLong("datetime")))
+            try (LiquidRecordDbHelper recordDbHelper = new LiquidRecordDbHelper(this))
             {
-                Toast.makeText(this, "Successful deleted.", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-            else
-            {
-                Toast.makeText(this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                // Call delete record function and check the result.
+                if (recordDbHelper.deleteRecord(bundle.getLong("datetime")))
+                {
+                    Toast.makeText(this, "Successful deleted.", Toast.LENGTH_SHORT).show();
+                    Intent output = new Intent(RecordItemPopUp.this, MainActivity.class);
+                    output.putExtra("delete", bundle.getInt("position"));
+                    setResult(RESULT_OK, output);
+                    finish();
+                } else
+                {
+                    Toast.makeText(this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
     }
 }
